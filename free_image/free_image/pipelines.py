9b2e3edbe627ed4025670214e680838b596fb2f9
miumiu0917 <miumiu917@gmail.com>
2017-07-23 07:38:04 +0000

    updated

--- free_image/free_image/pipelines.py
+++ free_image/free_image/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+sys.path.append('./model')
+from repository import Repository
 
 class FreeImagePipeline(object):
+    def __init__(self):
+        self.repository = Repository()
+
     def process_item(self, item, spider):
+        self.repository(item)
         return item
--- free_image/free_image/spiders/pakutaso.py
+++ free_image/free_image/spiders/pakutaso.py
@@ -4,11 +4,22 @@ import scrapy
 
 class PakutasoSpider(scrapy.Spider):
     name = 'pakutaso'
-    allowed_domains = ['https://www.pakutaso.com/']
-    start_urls = ['http://https://www.pakutaso.com//']
-
+    allowed_domains = ['www.pakutaso.com']
+    start_urls = ['https://www.pakutaso.com/']
+    
     def parse(self, response):
-        pass
+        anchors = response.css("a")
 
+        image_urls = [anchor.css("a::attr(href)").extract_first() for anchor in anchors if anchor.css("a::attr(data-category)").extract()]
+        for image_url in image_urls:
+            yield scrapy.Request(image_url, callback=self.parse_dir_contents)
+
+        next_pages = [anchor.css("a::attr(href)").extract_first() for anchor in anchors if not anchor.css("a::attr(data-category)").extract()]
+        
+        for next_page in next_pages:
+            url = response.urljoin(next_page)
+            yield scrapy.Request(url, callback=self.parse)
+    
     def parse_dir_contents(self, response):
-        pass
+        items = FreeImageItem()
+        return items
